6345ebea0cb7d7001554bc1fefa5e1ddaaccf8cb
Michal Kejzlar <kejzlarmichal@gmail.com>
2016-02-24 14:27:24 +0100

    Delete timeout, add comments

--- takeout-download.ps1
+++ takeout-download.ps1
@@ -1,7 +1,9 @@
+# change this
 $login = 'put_your_login_here'
 $password = 'put_your_password_here'
 
-$server = 'dev.realpad.eu'
+# change this only if explicitly told
+$server = 'cms.realpad.eu'
 $api_version = 'v10'                                                  
 $postparams = @{login=$login;password=$password}
 
@@ -9,5 +11,5 @@ $endpoints = @("list-excel-customers", "list-excel-projects", "list-excel-produc
 
 foreach ($endpoint in $endpoints) {
     $filename = "$endpoint $(get-date -f yyyy-MM-dd-HH-mm).xls" 
-    Invoke-RestMethod -Uri "https://$server/ws/$api_version/$endpoint" -TimeoutSec 60 -Method POST -Body $postparams -OutFile $filename
+    Invoke-RestMethod -Uri "https://$server/ws/$api_version/$endpoint" -Method POST -Body $postparams -OutFile $filename
 }
